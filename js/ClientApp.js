245624a597ccea43069136d4658d7e9234f03cb0
jogacommuri@yahoo.com <joga@Jogas-MacBook-Pro.local>
2017-06-04 23:29:18 -0700

    commint2

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,24 +1,15 @@
-var div = React.DOM.div
-var h1 = React.DOM.h1
-var h2 = React.DOM.h2
+/* global React ReactDOM */
 
-var MyTitle = React.createClass({
-  render (){
-    return (
-      div(null,
-        h1({style: {color: this.props.color}},this.props.title)
-      )
-    )
-  }
-})
+var div = React.DOM.div
+var MyTitle = require('./MyTitle')
 
 var MyTitleFact = React.createFactory(MyTitle)
 
 var MyFirstComponent = (
   div(null,
-    MyTitleFact({title:'Props are great!',color: 'rebeccapurple'}),
-    React.createElement(MyTitle,{title:'Use props everywhere!', color: 'mediumaquamarine'}),
-    React.createElement(MyTitle,{title: 'Props are the best!',color: 'peru'})
+    MyTitleFact({ title: 'Props are great!', color: 'rebeccapurple'}),
+    React.createElement(MyTitle, { title: 'Use props everywhere!', color: 'mediumaquamarine'}),
+    React.createElement(MyTitle, { title: 'Props are the best!', color: 'peru'})
   )
 )
 
